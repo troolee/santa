ced571d2b5c610f8569dd822738cf41522a4a4b3
Pavel Reznikov <pashka.reznikov@gmail.com>
2019-12-03 22:14:31 -0800

    fix footer

--- app/src/components/Footer/Footer.tsx
+++ app/src/components/Footer/Footer.tsx
@@ -7,21 +7,30 @@ interface IProps {
   onLogout?: () => void;
 }
 
-export const Footer: React.SFC<IProps> = ({children, onLogout}) => (
-  <>
-    <div className="has-text-right foot" style={{padding: '0.5rem 1rem'}}>
-      <p className="is-size-6 bottom-menu">
-        <span>
-          <Link to="/">Home</Link>
-          <> | </><Link to="/privacy">Privacy Policy</Link>
-          <> | </><Link to="/terms">Terms and Conditions</Link>
-          {onLogout && <><> | </> <a onClick={onLogout} href="/">Logout</a></>}
-        </span>
-      </p>
-      <p className="is-size-7">
-        Made with ❤ in Canada by <a href="https://uglyunicorn.ca">Ugly Unicorn</a>
-      </p>
-      {children && <p className="is-size-7">{children}</p>}
-    </div>
-  </>
-);
+export const Footer: React.SFC<IProps> = ({children, onLogout}) => {
+  const onLogoutHandler = (e: any) => {
+    e.preventDefault();
+    if (onLogout) {
+      onLogout();
+    }
+    return false;
+  }
+  return (
+    <>
+      <div className="has-text-right foot" style={{padding: '0.5rem 1rem'}}>
+        <div className="is-size-6 bottom-menu">
+          <span>
+            <Link to="/">Home</Link>
+            <> | </><Link to="/privacy">Privacy Policy</Link>
+            <> | </><Link to="/terms">Terms and Conditions</Link>
+            {onLogout && <><> | </> <a onClick={onLogoutHandler} href="/">Logout</a></>}
+          </span>
+        </div>
+        <div className="is-size-7">
+          Made with ❤ in Canada by <a href="https://uglyunicorn.ca">Ugly Unicorn</a>
+        </div>
+        {children && <div className="is-size-7">{children}</div>}
+      </div>
+    </>
+  );
+};
