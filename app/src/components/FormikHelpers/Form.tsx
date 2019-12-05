4f0b0473bace111353ce616c28a1834e0dfe18b3
Pavel Reznikov <pashka.reznikov@gmail.com>
2019-12-05 13:00:27 -0800

    forms workaround

--- app/src/components/FormikHelpers/Form.tsx
+++ app/src/components/FormikHelpers/Form.tsx
@@ -13,8 +13,7 @@ interface IProps<P> {
 };
 
 export default class Form<P = {}> extends React.Component<IProps<P>> {
-  private refSubmitButton = React.createRef<HTMLInputElement>();
-  private workDoneNotifiers: Array<() => void> = [];
+  private formik: any | null = null;
 
   public render() {
     const initialValues = this.props.initialValues || {};
@@ -24,41 +23,34 @@ export default class Form<P = {}> extends React.Component<IProps<P>> {
         const data = this.props.validationSchema.cast(v);
         await Promise.resolve(this.props.onSubmit(data as any));
       }
-      this.workDoneNotifiers.forEach(done => done());
-      this.workDoneNotifiers = [];
+    };
+
+    // This solution is ugly as fuck, but for now it works. Needs to be re-thinking thought.
+    const prepareFormik = (fp: any) => {
+      this.formik = fp;
+      return fp;
     };
 
     return (
       <Formik initialValues={initialValues} validationSchema={this.props.validationSchema} onSubmit={onSubmit}>
         {
           fp => (
-            <>
-              <input type="submit" ref={this.refSubmitButton} onClick={fp.submitForm} style={{display: 'none'}} />
-              <FormContext.Provider
-                value={{
-                  formikProps: fp,
-                  isHorizontal: this.props.isHorizontal,
-                  labelWidth: this.props.labelWidth,
-                }}
-              >
-                {this.props.children}
-              </FormContext.Provider>
-            </>
+            <FormContext.Provider
+              value={{
+                formikProps: prepareFormik(fp),
+                isHorizontal: this.props.isHorizontal,
+                labelWidth: this.props.labelWidth,
+              }}
+            >
+              {this.props.children}
+            </FormContext.Provider>
           )
         }
       </Formik>
     );
   }
 
-  public submit() {
-    return new Promise(done => {
-      if (this.refSubmitButton.current) {
-        this.workDoneNotifiers.push(done);
-        this.refSubmitButton.current.click();
-      }
-      else {
-        done();
-      }
-    });
+  public async submit() {
+    return await this.formik.submitForm();
   }
 }
