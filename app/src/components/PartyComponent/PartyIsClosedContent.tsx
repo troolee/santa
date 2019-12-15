46e086a90ee5390d4d926ff44a73a3a219426b1a
Pavel Reznikov <pashka.reznikov@gmail.com>
2019-12-14 21:16:14 -0800

    last touch :)

--- app/src/components/PartyComponent/PartyIsClosedContent.tsx
+++ app/src/components/PartyComponent/PartyIsClosedContent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IParty, IUser } from '../../interfaces';
-import { Button } from '..';
+import ConfettiButton from '../ConfettiButton';
 
 interface IProps {
   party: IParty;
@@ -14,7 +14,10 @@ const PartyIsClosedContent: React.SFC<IProps> = ({party, user}) => {
       <p>{party.participantCount} awesome people are participating in this. Our very special elf finished his
         complex math and you know what? Each of you got one name. This is a person you have to get a gift to.</p>
       <p>Just ask the elf but keep the name in secret, nobody should know what is it. </p>
-      <p style={{marginBottom: 50}}>Have fun! And Happy Holidays!</p>
+      <p>Have fun! And Happy Holidays!</p>
+      <p style={{marginBottom: 50}}>
+        <ConfettiButton isColor="primary">&#x1F929; Yey! Party!</ConfettiButton>
+      </p>
     </>
   );
 }
