abb25a20ad19340b729b62dbd951be527b994881
Pavel Reznikov <pashka.reznikov@gmail.com>
2019-12-15 10:40:28 -0800

    typo

--- app/src/components/PartyComponent/PartyIsClosedContent.tsx
+++ app/src/components/PartyComponent/PartyIsClosedContent.tsx
@@ -10,7 +10,7 @@ interface IProps {
 const PartyIsClosedContent: React.SFC<IProps> = ({party, user}) => {
   return (
     <>
-      <p>Hey, {user.name}! This is a new stage of the {party.name}! You are oficially an Anonymous Ded Moroz! Hooray! &#x1F389;</p>
+      <p>Hey, {user.name}! This is a new stage of the {party.name}! You are officially an Anonymous Ded Moroz! Hooray! &#x1F389;</p>
       <p>{party.participantCount} awesome people are participating in this. Our very special elf finished his
         complex math and you know what? Each of you got one name. This is a person you have to get a gift to.</p>
       <p>Just ask the elf but keep the name in secret, nobody should know what is it. </p>
