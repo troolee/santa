11319b71a1f41f058c15c64783fe5c6ae81865cd
Pavel Reznikov <pashka.reznikov@gmail.com>
2021-10-19 16:26:11 -0700

    add status

--- app/src/components/Footer/Footer.tsx
+++ app/src/components/Footer/Footer.tsx
@@ -24,6 +24,7 @@ export const Footer: React.SFC<IProps> = ({children, onLogout, noProfile}) => {
             <Link to="/">Home</Link>
             <> | </><Link to="/privacy">Privacy Policy</Link>
             <> | </><Link to="/terms">Terms and Conditions</Link>
+            <> | </><a href="https://status.uglyunicorn.ca">Status</a>
             {onLogout && !noProfile && <><> | </> <Link to="/me">My Profile</Link></>}
             {onLogout && <><> | </> <a onClick={onLogoutHandler} href="/">Logout</a></>}
           </span>
